93b1cc55ac776944eb4c127a53c72c0c6bb8ac2f
Heintz-Boerje <103048965+Heintz-Boerje@users.noreply.github.com>
2023-02-08 04:36:34 -0500

    Add files via upload

--- packages/qtile.scm
+++ packages/qtile.scm
@@ -1,12 +1,15 @@
-(use-modules (guix packages qtile))
+(use-modules (guix packages) (gnu packages base))
+(use-modules (guix download))
+(use-modules (guix build-system python))
+(use-modules (guix licenses))
+(use-modules (gnu packages wm))
 
-(define qtile-0.22.1
-  (package
-   (inherit qtile)
-   (version "0.22.1")
-   (source (origin
-            (method url-fetch)
-            (uri (pypi-uri "qtile" version))
-            (sha256
-             (base32
-              "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7"))))
+(package
+    (inherit qtile)
+    (version "0.22.1")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "qtile" version))
+              (sha256
+               (base32
+                "12wg8y33xgb0x0gd9xfylxss97p97dy1cy52yln64493fi6wphr7")))))
